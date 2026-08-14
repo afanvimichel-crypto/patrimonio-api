0bdcb5d6cf12b8ab7fd61ca7342520351a1d9fcd
Kossi <afanvi.michel@gmail.com>
2026-08-13 22:07:30 -0300

    construtor

--- src/main/java/br/com/senai/patrimonio/PatrimonioApplication.java
+++ src/main/java/br/com/senai/patrimonio/PatrimonioApplication.java
@@ -1,6 +1,7 @@
 package br.com.senai.patrimonio;
 
 import br.com.senai.patrimonio.model.Empresa;
+import br.com.senai.patrimonio.model.Endereco;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,6 +14,31 @@ public class PatrimonioApplication {
 		Empresa empresa = new Empresa();
 		empresa.setRazaoSocial("Senai LTDA");
 		System.out.println(empresa.getRazaoSocial());
+
+		Endereco endereco=new Endereco();
+		endereco.setRua("R. Gen. Lauro Sodré, ");
+		endereco.setNumero("300");
+		endereco.setComplemento("Blocos");
+		endereco.setBairro("Comerciario");
+		endereco.setCidade("Criciúma");
+		endereco.setEstado("Santa Catarina");
+		System.out.print(endereco.getRua());
+		System.out.println(endereco.getNumero());
+		System.out.println(endereco.getComplemento());
+		System.out.println(endereco.getBairro());
+		System.out.println(endereco.getCidade());
+		System.out.println(endereco.getEstado());
+
+		System.out.println("******************\n");
+		Endereco enderecocomArgumento=new Endereco("Archangelo Meller, ","1432","casa","Mineira Velha","Criciúma",",SC");
+		System.out.print(enderecocomArgumento.getRua());
+		System.out.println(enderecocomArgumento.getNumero());
+		System.out.println(enderecocomArgumento.getComplemento());
+		System.out.println(enderecocomArgumento.getBairro());
+		System.out.print(enderecocomArgumento.getCidade());
+		System.out.println(enderecocomArgumento.getEstado());
+
+
 	}
 
 
